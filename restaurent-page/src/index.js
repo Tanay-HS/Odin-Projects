579b0d6575bbad354d4c7c6318ef030d42f15955
Tanay HS Kolekar <tanay.hsk.study@gmail.com>
2026-06-18 00:31:38 +0530

    Added JS logic for tabs

--- restaurent-page/src/index.js
+++ restaurent-page/src/index.js
@@ -1,14 +1,28 @@
-
+import './style.css';
 
 import { loadHome } from './home-page/home.js';
 import loadAbout  from './about-page/about.js'
 import loadEvent from './events-page/event.js'
+document.querySelector('#content').appendChild(loadHome());
 
+function handleClick(e){
+    const contentDiv = document.querySelector('#content');
 
-const contentDiv = document.querySelector('#content');
+    let load;
+    const buttonClicked = e.target;
+    if(buttonClicked.id === "home") load = loadHome();
+    else if(buttonClicked.id === "about") load = loadAbout();
+    else load = loadEvent();
 
+    contentDiv.textContent='';
+    contentDiv.appendChild(load);
+}
 
-const homeTab = loadHome();
-const aboutTab = loadAbout();
-const eventTab = loadEvent();
-contentDiv.appendChild(eventTab);
+const  homeBtn = document.querySelector("#home");
+homeBtn.addEventListener('click', (e)=>handleClick(e));
+
+const  eventBtn = document.querySelector("#event");
+eventBtn.addEventListener('click', (e)=>handleClick(e));
+
+const  aboutBtn = document.querySelector("#about");
+aboutBtn.addEventListener('click', (e)=>handleClick(e));
